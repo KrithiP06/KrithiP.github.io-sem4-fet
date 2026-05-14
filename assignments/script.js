9732cabb51c6ca4dd7e1b96ef3ade370c7c23a24
Krithi Prakash <19736krithi@gmail.com>
2026-05-14 22:50:18 +0530

    Add functions to manage layer visibility and output

--- assignments/script.js
+++ assignments/script.js
@@ -1,1 +1,46 @@
+function resetLayers() {
+    document.getElementById("red").style.zIndex = "0";
+    document.getElementById("blue").style.zIndex = "0";
+    document.getElementById("green").style.zIndex = "0";
+}
 
+function showRed() {
+    resetLayers();
+    document.getElementById("red").style.zIndex = "3";
+}
+
+function showBlue() {
+    resetLayers();
+    document.getElementById("blue").style.zIndex = "3";
+}
+
+function showGreen() {
+    resetLayers();
+    document.getElementById("green").style.zIndex = "3";
+}
+
+function autoSwitch() {
+
+    let colors = ["Red", "Blue", "Green"];
+    let i = 0;
+    let text = "";
+
+    while(i < colors.length) {
+        text += colors[i] + " Layer<br>";
+        i++;
+    }
+
+    document.getElementById("output").innerHTML =
+        "While Loop Output:<br>" + text;
+
+    let j = 1;
+    let count = "";
+
+    do {
+        count += "Layer " + j + " Active<br>";
+        j++;
+    } while(j <= 3);
+
+    document.getElementById("output").innerHTML +=
+        "<br>Do While Output:<br>" + count;
+}
